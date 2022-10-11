87d925a3b083c0aeb914039ea3a03cffcc7a4a83
v.nichnikov <v.nichnikov@vk.team>
2022-10-11 23:13:10 +0300

    Внесены корректировки в тесты

--- tests/test_request_handler.py
+++ tests/test_request_handler.py
@@ -1,34 +1,29 @@
+import json
+
 import pytest
 from starlette.testclient import TestClient
 
-from app import fast_app
-from app.schemes.request_schemas import UserResponse
+from app.commands import fast_app
 
-user_response_correct = UserResponse(user_id="test_user_id",
-                                     name_block="level_1",
-                                     number_question_in_block=2,
-                                     answer_user=3)
+user_response_correct = {"user_id": "test_user_id",
+                         "name_block": "level_1",
+                         "number_question_in_block": 2,
+                         "answer_id": 3}
 
-user_response_with_invalid_block_name = UserResponse(
-    user_id="test_user_id",
-    name_block="level_45",
-    number_question_in_block=2,
-    answer_user=3
-)
+user_response_with_invalid_block_name = {"user_id": "test_user_id",
+                                         "name_block": "level_45",
+                                         "number_question_in_block": 2,
+                                         "answer_id": 3}
 
-user_response_with_incorrect_question_number_in_block = UserResponse(
-    user_id="test_user_id",
-    name_block="level_1",
-    number_question_in_block=-14,
-    answer_user=3
-)
+user_response_with_incorrect_question_number_in_block = {"user_id": "test_user_id",
+                                                         "name_block": "level_1",
+                                                         "number_question_in_block": -14,
+                                                         "answer_id": 3}
 
-user_response_with_incorrect_type_answer_user = UserResponse(
-    user_id="test_user_id",
-    name_block="level_1",
-    number_question_in_block=2,
-    answer_user="answer_user"
-)
+user_response_with_incorrect_type_answer_user = {"user_id": "test_user_id",
+                                                 "name_block": "level_1",
+                                                 "number_question_in_block": -14,
+                                                 "answer_id": "answer_user"}
 
 
 @pytest.fixture(scope="function")
@@ -49,7 +44,7 @@ def test_check_answer_user_with_correct_user_response(setup) -> None:
     client = setup
 
     # Вызов тестируемой функции
-    response_check_answer_user = client.post("/check_answer_user", user_response_correct)
+    response_check_answer_user = client.post("/check_answer_user", data=json.dumps(user_response_correct))
 
     # Тестирование результатов
     assert response_check_answer_user.status_code == 200
@@ -68,7 +63,8 @@ def test_check_answer_user_with_invalid_block_name(setup) -> None:
     client = setup
 
     # Вызов тестируемой функции
-    response_check_answer_user = client.post("/check_answer_user", user_response_with_invalid_block_name)
+    response_check_answer_user = client.post("/check_answer_user",
+                                             data=json.dumps(user_response_with_invalid_block_name))
 
     # Тестирование результатов
     assert response_check_answer_user.status_code == 200
@@ -88,7 +84,7 @@ def test_check_answer_user_with_incorrect_question_number_in_block(setup) -> Non
 
     # Вызов тестируемой функции
     response_check_answer_user = client.post("/check_answer_user",
-                                             user_response_with_incorrect_question_number_in_block)
+                                             data=json.dumps(user_response_with_incorrect_question_number_in_block))
 
     # Тестирование результатов
     assert response_check_answer_user.status_code == 200
@@ -107,9 +103,9 @@ def test_check_answer_user_with_incorrect_type_answer_user(setup) -> None:
     client = setup
 
     # Вызов тестируемой функции
-    response_check_answer_user = client.post("/check_answer_user", user_response_with_incorrect_type_answer_user)
+    response_check_answer_user = client.post("/check_answer_user",
+                                             data=json.dumps(user_response_with_incorrect_type_answer_user))
 
     # Тестирование результатов
-    assert response_check_answer_user.status_code == 200
+    assert response_check_answer_user.status_code == 422
     assert response_check_answer_user.json() == {"status": "error", "answer": "Invalid data type"}
-
